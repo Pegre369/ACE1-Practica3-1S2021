03c2c3db7c9f04b03f8c963249bb44afc8cbca01
SebastianSio <ssebastianhchacon@gmail.com>
2021-03-22 17:55:36 -0600

    Modo Calculadora
    -Modo calculadora funcional
    -Falta id y función de guardar al modo calculadora
    -Inicio de modo factorial

--- Main.asm
+++ Main.asm
@@ -1,101 +1,87 @@
-include macros.asm ;Importar el archivo "macros.asm"
+include Macros.asm  ;Import file "macros.asm"
+
 .model small
-.stack 64h
+.stack 100h
 .data
-    ;Mensaje de encabezado
-    encabezado  db 13,10,"UNIVERSIDAD DE SAN CARLOS DE GUATEMALA"
-                db 13,10,"FACULTAD DE INGENIERIA"
-                db 13,10,"ESCUELA DE CIENCIAS Y SISTEMAS"
-                db 13,10,"ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1"
-                db 13,10,"SECCION A"
-                db 13,10,"PRIMER SEMESTRE 2021"
-                db 13,10,"Sergio Sebastian Chacon Herrera"
-                db 13,10,"201709159","$"
-    ;Mensaje para el menu principal
-    menu    db 13,10,"------MENU PRINCIPAL------"
-            db 13,10,"Opciones:"
-            db 13,10,"1. Cargar Archivo"
-            db 13,10,"2. Modo Calculadora"
-            db 13,10,"3. Factorial"
-            db 13,10,"4. Crear Reporte"
-            db 13,10,"5. Salir"
-            db 13,10,"Elegir: ",'$'
-    ;Mensaje para el menu modo calculadora
-    menucalc  db 13,10,"-----MODO CALCULADORA-----","$"
-    ;Mensaje ingresar numero
-    ingnum    db 13,10,"Ingresar Numero:","$"
-    ;Mensaje ingerar operador
-    ingop     db 13,10,"Ingresar Operador:","$"
-    ;Mensaje ingresar operador o finalizar
-    ingopfin  db 13,10,"Ingresar Operador o ; para finalizar:","$"
-    ;Mensaje resultado
-    menres    db 13,10,"El resultado es:","$"
-    ;Mensaje opcion guardar
-    mensave   db 13,10,"Desea guardar (S/N)","$"
-    mensaven  db 13,10,"No se guardo la operacion","$"
-    mensaves  db 13,10,"Se guardo la operacion","$"
-    ;Mensaje menu factorial
-    menufact db 13,10,"---------FACTORIAL--------","$"
-    ;Mensaje para menu abrir archivo
-    menuarchivo db 13,10,"---------CARGAR ARCHIVO--------","$"
-    ;Mensaje para seleccionar archivo
-    menarchivo db 13,10,"Ingresar ruta del archivo: ","$"
-    ;Mensaje de reporte
-    menrep db 13,10,"Reporte creado","$"
-    menerror db 13,10,"Error","$"
-
-    mensum db 13,10,"Suma: ","$"
-    menresta db 13,10,"Resta: ","$"
-    menmul db 13,10,"Multiplicacion: ","$"
-    mendiv db 13,10,"Division: ","$"
-    numero1 db ?
-    numero2 db ?
-
+    ;Text for header************************************************************
+    header  db 13,10,"UNIVERSIDAD DE SAN CARLOS DE GUATEMALA"
+            db 13,10,"FACULTAD DE INGENIERIA"
+            db 13,10,"ESCUELA DE CIENCIAS Y SISTEMAS"
+            db 13,10,"ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1"
+            db 13,10,"SECCION A"
+            db 13,10,"PRIMER SEMESTRE 2021"
+            db 13,10,"Sergio Sebastian Chacon Herrera"
+            db 13,10,"201709159","$"
+    ;Text for main menu*********************************************************
+    mainmenu    db 13,10,"------MENU PRINCIPAL------"
+                db 13,10,"Opciones:"
+                db 13,10,"1. Cargar Archivo"
+                db 13,10,"2. Modo Calculadora"
+                db 13,10,"3. Factorial"
+                db 13,10,"4. Crear Reporte"
+                db 13,10,"5. Salir"
+                db 13,10,"Elegir: ",'$'
+    ;Texts for menu Modo Calculadora********************************************
+    calcmenu  db 13,10,"-----MODO CALCULADORA-----","$"
+    promptnum   db 13,10,"Ingresar Numero:","$"
+    promptop    db 13,10,"Ingresar Operador:","$"
+    promptcont  db 13,10,"Ingresar Operador o ; para finalizar:","$"
+    mesres    db 13,10,"El resultado es: ","$"
+    messave   db 13,10,"Desea guardar (S/N)","$"
+    messaven  db 13,10,"No se guardo la operacion","$"
+    messavey  db 13,10,"Se guardo la operacion","$"
+    meserr db 13,10,"Error","$"
+    mesadd db 13,10,"Suma: ","$"
+    messub db 13,10,"Resta: ","$"
+    mesmul db 13,10,"Multiplicacion: ","$"
+    mesdiv db 13,10,"Division: ","$"
+    number1 db 5 dup ("$")
+    number2 db 5 dup ("$")
+    number1n dw ?
+    number2n dw ?
+    resultado dw ?
+    ;Texts for menu Modo Factorial
+    factmenu db 13,10,"---------FACTORIAL--------","$"
+    ;Texts for menu Cargar Archivo
+    filemenu db 13,10,"---------CARGAR ARCHIVO--------","$"
+    promptfile db 13,10,"Ingresar ruta del archivo: ","$"
+    ;Texts for menu Reportes
+    mesrep db 13,10,"Reporte creado","$"
+    ;Miscellaneous memory cells
+    
 .code
-main proc ;Inicio procedimiento principal
-    ClearScreen
-    Imprimir encabezado
+main proc   ;Main procedure starts
+    mov ax,@data
+    mov ds,ax
+    ;ClearScreen
+
+    PrintText header
     start:
-        Imprimir menu
-        EntradaTeclado ;Ingresar opcion
-        mov bl,al
-        case1:;Cargar Archivo
-            cmp bl,"1" ;Comparar si registro bl es igual a 1
-            jne case2 ;bl no es 1, ir a "case2"
-            Imprimir menuarchivo
-            Imprimir menarchivo
-
+        PrintText mainmenu
+        EnterOption
+        mov bl,al   ;Move input to register "bl"
+        case1:  ;Cargar Archivo
+            cmp bl,"1" ;Compare if bl is 1
+            jne case2 ;bl is not 1, go to case2
+        case2:  ;Modo Calculadora
+            cmp bl,"2" ;Compare if bl is 2
+            jne case3 ;bl is not 2, go to case3
+            ModoCalculadora ;bl is 2, start ModoCalculadora
+            jmp start   ;Jump to start
+        case3:  ;Factorial
+            cmp bl,"3" ;Compare if bl is 3
+            jne case4 ;bl is not 3, go to case4
+        case4:  ;Crear Reporte
+            cmp bl,"4" ;Compare if bl is 4
+            jne case5 ;bl is not 4, go to case5
+        case5:  ;Salir
+            cmp bl,"5" ;Compare if bl is 5
+            jne case6 ;bl is not 5, go to case6
+            mov ah,4ch  ;Exit program
+            int 21h     ;Exit program
+        case6:  ;Any other case
             ClearScreen
-            jmp start ;Regresar a menu principal
-        case2:;Modo Calculadora
-            cmp bl,"2" ;Comparar si registro bl es igual a 2
-            jne case3 ;bl no es 2, ir a "case3"
-            ClearScreen
-            ModoCalculadora ;Inicia el modo calculadora
-            jmp start ;Regresar a menu principal
-        case3:;Factorial
-            cmp bl,"3" ;Comparar si registro bl es igual a 3
-            jne case4 ;bl no es 3, ir a "case4"
-            Imprimir menufact
-
-            ClearScreen
-            jmp start ;Regresar a menu principal
-        case4:;Crear Reporte
-            cmp bl,"4" ;Comparar si registro bl es igual a 4
-            jne case5 ;bl no es 4, ir a "case4"
-            ClearScreen
-            Imprimir menrep
-            jmp start ;Regresar a menu principal
-        case5:;Salir del programa
-            cmp bl,"5" ;Comparar si registro bl es igual a 5
-            jne case6 ;bl no es 5, ir a "case6"
-            ClearScreen
-            mov ah,4ch 
-            int 21h ;Salir del programa
-        case6:
-            ClearScreen
-            jmp start;En cualquier otro caso regresar al menu principal
-
-main endp ;Fin procedimiento principal
-
+            jmp start   ;Jump to start
+            
+main endp   ;Main procedure ends
 end main
--- macros.asm
+++ macros.asm
@@ -1,103 +1,271 @@
-Imprimir macro Cadena ;Macro para imprimir "Cadena" en la pantalla
-    mov ax,@data
-    mov ds,ax
-    mov ah,09h
-    lea dx,Cadena
-    int 21h
-endm
-ClearScreen macro ;Macro para limpiar pantalla
+ClearScreen macro   ;Clears the screen
   mov  ah, 0
   mov  al, 3
   int  10H
 endm
-EntradaTeclado macro ;Macro para entradas en el teclado
-    mov ah,01
+PrintText macro Text    ;Prints "Text"
+    mov ax,@data
+    mov ds,ax
+    mov ah,09h
+    lea dx,Text
     int 21h
 endm
-ReadInteger macro;Leer un numero
-    mov ah,1    ;Modo entrada
-    int 21h     ;Modo entrada
-endm
-PrintRegistro macro Num ;Macro para imprimir un registro
+PrintRegister macro Register    ;Prints "Register"
     xor ax,ax
-    mov dl,Num
+    mov dl,Register
     mov ah,02h
     int 21h
 endm
-ModoCalculadora macro ;Macro para modo calculadora
-    Imprimir menucalc
-    Imprimir ingnum;Ingresar numero
-    ReadInteger
-    mov bl,al;Almacenar el primer valor
-    Imprimir ingop;Ingresar operacion
-    EntradaTeclado
-    mov ch,al;Almacenar el operador
-    Imprimir ingnum;Ingresar segundo numero
-    ReadInteger
-    mov cl,al;Alamcenar el segundo valor
-    operar:;Realizar operacion
-        cmp ch,"+"
-        jne resta
-        jmp suma
-    ciclo:
-        Imprimir ingopfin
-        EntradaTeclado
+EnterOption macro   ;Read input for options
+    mov ah,01   ;Input mode
+    int 21h     ;Input mode
+endm
+Print16 macro Regis ;Print a 16bit number
+    local zero,noz
+    mov bx,4
+    xor ax,ax
+    mov ax,Regis
+    mov cx,10
+    zero:
+        xor dx,dx
+        div cx
+        push dx
+        dec bx
+        jnz zero
+        xor bx,4
+    noz:
+        pop dx
+        PrintN dl
+        dec bx
+        jnz noz
+endm
+PrintN macro Num    ;Print a digit
+    xor ax,ax
+    mov dl,Num
+    add dl,48
+    mov ah,02h
+    int 21h
+endm
+ReadText macro buffer   ;Reads input text
+    local read  ;Read mode
+    local fin   ;End of number
+    xor si,si   ;Clear si
+    read:
+        mov ah,1    ;Input mode
+        int 21h     ;Input mode
+        cmp al, 13  ;Compare if input is "ENTER"
+        je fin   ;If input is "ENTER" go to fin
+        mov buffer[si],al   ;Save input into buffer position si
+		inc si  ;Increment counter si
+        jmp read    ;Read next input
+    fin: ;End of number
+        mov al,00h
+		mov buffer[si],al   ;Save inout into buffer position si
+endm
+TextToDecimal macro buffer, des ;Converts text to decimal
+    Local start
+    Local fin
+    Local negative
+    Local positive
+    Local done
+    Local negate
+	xor ax,ax   ;Clears ax registry
+	xor bx,bx   ;Clears bx registry
+	xor cx,cx   ;Clears cs registry
+	xor di,di   ;Clears di resistry, 0 = Positive, 1 = Negative
+	mov bx,10	;Moves 10 into bx
+	xor si,si   ;Clears si registry, for counter of position inside buffer
+	start:
+		mov cl,buffer[si]   ;Move buffer in position si into cl
+		cmp cl,45   ;Compares if cl is "-"
+		je negative ;If cl is "-" jump to negative
+		jmp positive    ;If cl is not "-" jump to positive
+	negative:
+		inc di  ;Increment di to 1, now the number is negative
+		inc si  ;Increment si by 1 to read next value
+		mov cl,buffer[si]   ;Move the next value into cl
+	positive:
+		cmp cl,48   ;Compares if cl is 0
+		jl fin  ;Jump to negate
+		cmp cl,57   ;Compares if cl is 9
+		jg fin  ;Jump to negate
+		inc si  ;Increment si to read next value
+		sub cl,48	;Substract 48 to cl to get number
+		mul bx		;Multiply ax by bx
+		add ax,cx	;Add to ax cx
+		jmp start   ;Jump to start
+	fin:
+		cmp di,1    ;Compares if di = 1
+		je negate   ;Go to negate to negate ax
+		jmp done    ;If di = 0 go to done
+	negate:
+		neg ax  ;Negates ax
+	done:
+        mov des,ax  ;Moves register ax into des, which is output
+endm
+DecimalToText macro entrada, salida ;Converts decimal to text
+    Local divide
+    Local divide2
+    Local make
+    Local negative
+    Local done
+    xor ax,ax   ;Clear ax
+    mov ax,entrada  ;Move number into ax
+	xor si,si   ;Clear si
+	xor cx,cx   ;Clear cx
+	xor bx,bx   ;Clear bx
+	xor dx,dx   ;Clear dx
+	mov bx,0ah  ;Move 10 into bx
+	test ax,1000000000000000    ;Compare if ax is negative
+	jnz negative    ;If ax is negative go to negative
+	jmp divide2 ;if ax is positive go to divide2
+	negative:
+		neg ax  ;Negate ax to make it positive
+		mov salida[si],45   ;Move a "-" at the start of text
+		inc si  ;Increment counter si
+		jmp divide2    ;Go to divide 2
+	divide:
+		xor dx,dx   ;Clear dx
+	divide2:
+		div bx  ;divide ax by bx
+		inc cx  ;Increment counter cx
+		push dx ;Push dx register into stack
+		cmp ax,00h  ;Campre if ax is 0
+		je make ;IF ax is 0 go to make
+		jmp divide  ;If ax is not 0 go to divide
+	make:
+		pop ax  ;Take out last register from stack
+		add ax,30h  ;Make conversion
+		mov salida[si],ax   ;Move ax into salida position si
+		inc si  ;Increment counter si
+		loop make ;Loop to make
+		mov ax,24h  ;Move $ to ax
+		mov salida[si],ax   ;Move ax into salida position si
+		inc si  ;Increment si
+	done:
+		;PrintText salida    ;Display result
+endm
+;MACROS FOR MODO CALCULADORA***************************************************************
+ModoCalculadora macro   ;Enters "Modo Calculadora"
+    ;Loops used only in Modo Calculadora
+    local operate
+    local loop
+    local loop2
+    local saves
+    local saven
+    local saven2
+    local addition
+    local substract
+    local multiply
+    local divide
+    local fin
+    local err
+    ;Modo Calculadora begins
+    ClearScreen
+    PrintText calcmenu
+    PrintText promptnum
+    ReadText number1 ;Save number1
+    TextToDecimal number1, number1n
+    PrintText promptop
+    EnterOption
+    mov ch,al   ;Save operand into ch
+    PrintText promptnum
+    ReadText number2 ;Save number2
+    operate:    ;Label to check which operation to perform
+        cmp ch,"+"  ;Compare if ch is +
+        jne substract   ;If ch is not h jump to substract
+        jmp addition    ;Jump to addition
+    loop:
+        PrintText promptcont
+        EnterOption
         mov ch,al
-        cmp ch,";"
-        jne ciclo2
-        Imprimir menres
-        Imprimir mensave
-        EntradaTeclado
+        cmp ch,";"  ;Compare if ch is ;
+        jne loop2   ;ch is not ; so jump to loop2
+        PrintText mesres   ;ch is ; so it continues
+        PrintText resultado
+        PrintText messave
+        EnterOption
         mov bl,al
-        cmp bl,"s"
-        jne saven
-        jmp saves
-    ciclo2:
-        Imprimir ingnum
-        ReadInteger
-        mov cl,al;Alamcenar el segundo valor
-        jmp operar
-    saves:;Se guarda la operacion
+        cmp bl,"s"  ;Compare if bl is s
+        jne saven   ;bl is not s so jump to saven
+        jmp saves   ;bl is s no jump to saves
+    loop2:
+        PrintText promptnum
+        ReadText number2
+        jmp operate
+    saves:  ;Operation is saved
         ClearScreen
-        Imprimir mensaves
-        jmp fin
-    saven:;No se guarda la operacion
+        PrintText messavey
+        jmp fin ;Jumps to fin
+    saven:  ;Operation is not saved
+        cmp bl,"S"  ;Compare if bl is S
+        jne saven2  ;bl is not S so jump to saven2
         ClearScreen
-        Imprimir mensaven
-        jmp fin
-    suma:;El operador es una suma
-        add al,bl
-        mov ah,0
-        aaa
-        mov bx,ax
-        add bh,48
-        add bl,48
-        Imprimir mensum
-        mov ah,2
-        mov dl,bh
-        int 21h;Imprime el primer digito
-        mov ah,2
-        mov dl,bl
-        int 21h;Imprime el segundo digito
-        jmp ciclo
-    resta:;El operador es una resa
-        cmp ch,"-"
-        jne mult
-        Imprimir menresta
-        jmp ciclo
-    mult:;El operador es una multiplicacion
-        cmp ch,"*"
-        jne division
-        Imprimir menmul
-        jmp ciclo
-    division:;El operador es una division
-        cmp ch,"/"
-        jne err
-        Imprimir mendiv
-        jmp ciclo
+        PrintText messavey
+        jmp fin ;Jumps to fin
+    saven2:
+        ClearScreen
+        PrintText messaven
+        jmp fin ;Jumps to fin
+    addition:
+        MCAdd
+        jmp loop   ;Jumps to loop
+    substract:
+        cmp ch,"-"  ;Compares if ch is -
+        jne multiply    ;ch is not - so jump to multiply
+        MCSubstract
+        jmp loop   ;Jumps to loop
+    multiply:
+        cmp ch,"*"  ;Compares if ch is *
+        jne divide  ;ch is not * so jumpt to divide
+        MCMultiply
+        jmp loop    ;Jumps to loop 
+    divide:
+        cmp ch,"/"  ;Compares if ch is /
+        jne err ;ch is not / so jump to err
+        MCDivide
+        jmp loop    ;Jumps to loop
     err:
         ClearScreen
-        Imprimir menerror
-    fin:;finaliza el modo calculadora
-
+        PrintText meserr
+    fin:    ;Exits Modo Calculadora
 endm
+MCAdd macro
+    TextToDecimal number2, number2n
+    mov bx, number2n    ;Move number2n into bx
+    add number1n, bx    ;Add bx to number1n
+    DecimalToText number1n, resultado
+    ;PrintText mesadd
+    ;PrintText resultado
+endm
+MCSubstract macro
+    TextToDecimal number2, number2n
+    mov bx, number2n    ;Move number2n into bx
+    sub number1n, bx    ;Substract bx to number1n
+    DecimalToText number1n, resultado
+    ;PrintText messub
+    ;PrintText resultado
+endm
+MCMultiply macro
+    TextToDecimal number2, number2n
+    mov bx, number2n    ;Move number2n into bx
+    mov ax, number1n    ;Move number1n into ax
+    imul bx ;Multiply ax by bx
+    mov number1n, ax    ;Move ax into number1n
+    DecimalToText number1n, resultado
+    ;PrintText mesmul
+    ;PrintText resultado
+endm
+MCDivide macro
+    TextToDecimal number2, number2n
+    xor dx,dx   ;Clear dx
+    mov bx, number2n    ;Move number2n into bx
+    mov ax, number1n    ;Move number1n into ax
+    cwd ;Convert word to doubleword
+    idiv bx ;Divide ax by bx
+    mov number1n, ax    ;Move ax into number1n
+    DecimalToText number1n, resultado
+    ;PrintText mesdiv
+    ;PrintText resultado
+endm
+;******************************************************************************************
